634c107d8f25228e77e38766dbec9dda1b04542c
Meikle, Braeden Andrew <bmeikle3@gatech.edu>
2026-08-03 22:42:38 -0400

    initial commit

---
+++ maxwin/Maxwin/maxwinApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaxwinApp.swift
+//  Maxwin
+//
+//  Created by Braeden Meikle on 8/3/26.
+//
+
+import SwiftUI
+
+@main
+struct MaxwinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ maxwin/MaxwinTests/MaxwinTests.swift
@@ -0,0 +1,19 @@
+//
+//  MaxwinTests.swift
+//  MaxwinTests
+//
+//  Created by Braeden Meikle on 8/3/26.
+//
+
+import Testing
+@testable import Maxwin
+
+struct MaxwinTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
